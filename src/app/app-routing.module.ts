fc03daa9e140befcb327ebd9f5afaf5771c145ac
Christian Padierna <christianpadierna16@gmail.com>
2023-06-06 12:43:34 -0400

    Creating Routing
    -Moving onto page 2 - Homepage
    -Working on routing in Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: RegisterComponent
+  },
+  {
+    path: 'homepage',
+    component: HomepageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
